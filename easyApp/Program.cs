b9191d67974c2f2adf6c7e3acce36009bdcaa835
Gorgulu Codes <gorgulucodes@gmail.com>
2019-10-13 11:21:53 +0300

    asdasdadding my god tier project

---
+++ easyApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = Convert.ToInt32(Console.ReadLine());
+        }
+
+    }
+}
